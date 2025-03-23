9d6b4a9ec9515ade2927741569aa208195bd28f5
Gabfuwak <gabin.maury0@gmail.com>
2025-03-23 19:09:49 +0100

    feat: camera moves easing

--- src/ecs/systems/camera_system.ml
+++ src/ecs/systems/camera_system.ml
@@ -4,6 +4,12 @@ open Position
 open Camera
 open Vector
 
+let linear_easing current_pos target_pos =
+  let easing_factor = 0.05 in
+  let direction = Vector.sub target_pos current_pos in
+  let movement = Vector.scale direction easing_factor in
+  Vector.add current_pos movement
+
 let update world =
   match world.active_camera_id with
   | Some camera_id ->
@@ -14,7 +20,8 @@ let update world =
             | Some target_id ->
                 (match Hashtbl.find_opt world.state.position_store target_id with
                  | Some target_pos ->
-                     camera_pos.pos <- Vector.add target_pos.pos camera.offset
+                     let eased_pos = linear_easing camera_pos.pos target_pos.pos in
+                     camera_pos.pos <- Vector.add eased_pos camera.offset
                  | None -> ())
             | None -> ())
        | _, _ -> ())
